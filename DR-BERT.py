f06dbc8ac0b392c0695415837ac97c87814554b3
larsbb@clin.au.dk <shd-sun-lab@1874-127-001.uni.au.dk>
2025-11-13 17:50:51 +0100

    Analysis now work! Protein generation on its way...

--- DR-BERT.py
+++
@@ -1,5 +0,0 @@
-from transformers import AutoModelForTokenClassification, AutoTokenizer
-
-checkpoint = "checkpoints/drbert"
-tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-model = AutoModelForTokenClassification.from_pretrained(checkpoint)
--- ESM2.py
+++
@@ -1,3 +0,0 @@
-import torch
-torch.hub.set_dir("checkpoints/esm2")
-model, alphabet = torch.hub.load("facebookresearch/esm:main", "esm2_t6_8M_UR50D")
